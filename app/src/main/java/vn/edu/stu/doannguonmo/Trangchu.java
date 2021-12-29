40fd5a3a1989b5c9e8ce27999a47a5f7a0b0a4a1
ThuyHoai <lethithuyhoai0310@gmail.com>
2021-12-29 22:26:18 +0700

    Thêm menu

--- app/src/main/java/vn/edu/stu/doannguonmo/Trangchu.java
+++ app/src/main/java/vn/edu/stu/doannguonmo/Trangchu.java
@@ -1,14 +1,41 @@
 package vn.edu.stu.doannguonmo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class Trangchu extends AppCompatActivity {
+//    final String DB_PATH_SUFFIX = "/databases/";
+//    final String DB_NAME = "dbTS.sqlite";
+//    ListView lv;
+//    ArrayList<trasua> ds;
+//    listviewadapter lvadapter;
+//    FloatingActionButton fabThem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trangchu);
+
     }
 }
