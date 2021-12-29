884a1989683ffa3b660af6ecaed7d4f4821209b1
ThuyHoai <lethithuyhoai0310@gmail.com>
2021-12-29 22:27:44 +0700

    Thêm Adapter

--- app/src/main/java/vn/edu/stu/doannguonmo/Trangchu.java
+++ app/src/main/java/vn/edu/stu/doannguonmo/Trangchu.java
@@ -25,17 +25,159 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class Trangchu extends AppCompatActivity {
-//    final String DB_PATH_SUFFIX = "/databases/";
-//    final String DB_NAME = "dbTS.sqlite";
-//    ListView lv;
-//    ArrayList<trasua> ds;
-//    listviewadapter lvadapter;
-//    FloatingActionButton fabThem;
+    final String DB_PATH_SUFFIX = "/databases/";
+    final String DB_NAME = "dbTS.sqlite";
+    ListView lv;
+    ArrayList<trasua> ds;
+    listviewadapter lvadapter;
+    FloatingActionButton fabThem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trangchu);
+        copyDbFromAssets();
+        addControls();
+        addEvents();
+        docDssanpham();
+    }
 
+    private void docDssanpham() {
+        SQLiteDatabase database = openOrCreateDatabase(
+                DB_NAME,
+                MODE_PRIVATE,
+                null
+        );
+        ds.clear();
+        Cursor cursor = database.rawQuery("select * from NuocUong", null);
+        while (cursor.moveToNext()) {
+            int ma=cursor.getInt(0);
+            String ten=cursor.getString(1);
+            String phanloai=cursor.getString(2);
+            byte[] hinhanh=cursor.getBlob(3);
+            String gia=cursor.getString(4);
+            String soluong=cursor.getString(5);
+            trasua ts= new trasua(ma, ten, phanloai, hinhanh,gia, soluong);
+            ds.add(ts);
+        }
+        lvadapter = new listviewadapter(Trangchu.this, ds, R.layout.listview);
+        lv.setAdapter(lvadapter);
+        lvadapter.notifyDataSetChanged();
+        cursor.close();
+        database.close();
+
+    }
+    private void addControls() {
+        lv = findViewById(R.id.lvtrangchu);
+        ds = new ArrayList<>();
+        fabThem = findViewById(R.id.fabThem);
+    }
+
+    private void addEvents() {
+        fabThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Trangchu.this, ThemSP.class);
+                startActivity(intent);
+            }
+        });
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(Trangchu.this, ChiTiet.class);
+                intent.putExtra("intentChitiet",ds.get(position));
+                startActivity(intent);
+            }
+        });
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Trangchu.this);
+                builder.setIcon(android.R.drawable.ic_delete);
+                builder.setTitle("Thông báo");
+                builder.setMessage("Bạn có chắc muốn xóa sản phẩm này không?");
+                builder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        trasua ts = ds.get(position);
+                        SQLiteDatabase database = openOrCreateDatabase(
+                                DB_NAME,
+                                MODE_PRIVATE,
+                                null
+                        );
+                        int deletedRowCount = database.delete(
+                                "NuocUong",
+                                "Ma= ?",
+                                new String[]{ts.getMa() + ""}
+                        );
+                        Toast.makeText(
+                                Trangchu.this,
+                                "Xóa sản phẩm thành công!!!",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                        docDssanpham();
+                    }
+                });
+                builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return false;
+            }
+        });
+    }
+    private void copyDbFromAssets() {
+        File dbFile = getDatabasePath(DB_NAME);
+        if (!dbFile.exists()) {
+            try {
+                File dbDir = new File(getApplicationInfo().dataDir + DB_PATH_SUFFIX);
+                if (!dbDir.exists()) dbDir.mkdir();
+
+                InputStream is = getAssets().open(DB_NAME);
+                String outputFilePath = getApplicationInfo().dataDir + DB_PATH_SUFFIX + DB_NAME;
+                OutputStream os = new FileOutputStream(outputFilePath);
+                byte[] buffer = new byte[1024];
+                int length = 0;
+                while ((length = is.read(buffer)) > 0) {
+                    os.write(buffer, 0, length);
+                }
+                os.flush();
+                os.close();
+                is.close();
+            } catch (Exception ex) {
+                ex.fillInStackTrace();
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+//            case R.id.menuAbout:
+//                Intent about1= new Intent(Trangchu.this, About.class);
+//                startActivity(about1);
+//                break;
+            case R.id.menuListView:
+                Intent Listview= new Intent(Trangchu.this,Trangchu.class);
+                startActivity(Listview);
+                break;
+            case R.id.menuDanhMuc:
+                Intent danhmuc= new Intent(Trangchu.this,DanhMuc.class);
+                startActivity(danhmuc);
+                break;
+            case R.id.menuExit:
+                finishAffinity();
+                System.exit(0);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/vn/edu/stu/doannguonmo/adapter/listviewadapter.java
@@ -0,0 +1,96 @@
+package vn.edu.stu.doannguonmo.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import vn.stu.edu.ThuyHoai.ChiTiet;
+import vn.stu.edu.ThuyHoai.R;
+import vn.stu.edu.ThuyHoai.Sua;
+import vn.stu.edu.ThuyHoai.model.trasua;
+
+public class listviewadapter extends BaseAdapter {
+    public listviewadapter(Context context, ArrayList<trasua> traSua, int layout) {
+        this.context = context;
+        this.traSua = traSua;
+        this.layout = layout;
+
+        final  String DB_PATH_SUFFIX = "/databases/";
+        final String DB_NAME = "dbTS.sqlite";
+    }
+
+    Context context;
+    private ArrayList<trasua>traSua;
+    int layout;
+
+    @Override
+    public int getCount() {
+        return traSua.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return traSua.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(layout, null);
+        TextView txtName = convertView.findViewById(R.id.tvTenSP);
+        TextView txtGia = convertView.findViewById(R.id.tvGia);
+        ImageView imgHinh = convertView.findViewById(R.id.imgSanPham);
+        trasua trasua = traSua.get(position);
+        txtName.setText(trasua.getTen());
+        txtGia.setText(String.valueOf(trasua.getGia()));
+        byte[] img = trasua.getHinhanh();
+        Bitmap bitmap = BitmapFactory.decodeByteArray(img, 0, img.length);
+        imgHinh.setImageBitmap(bitmap);
+
+        ImageButton imgSua = convertView.findViewById(R.id.imgSua);
+        imgSua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        context,
+                        Sua.class
+                );
+                intent.putExtra("intentChitiet",traSua.get(position));
+                context.startActivity(intent);
+            }
+        });
+        ImageView imgSanPham = convertView.findViewById(R.id.imgSanPham);
+        imgSanPham.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        context,
+                        ChiTiet.class
+                );
+                intent.putExtra("intentChitiet",traSua.get(position));
+                context.startActivity(intent);
+            }
+        });
+        imgSanPham.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return false;
+            }
+        });
+        return convertView;
+    }
+}
